de4c79003a6a3554393a0c1e33749838c71d6b34
nbwong <nicholas.b.wong@gmail.com>
2022-11-06 11:39:06 -0800

    updated App.js, PortfolioContainer.js, About.js, and index.js

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import Footer from './Footer';
+import Header from './Header';
+import Navigation from './Navigation';
+import Project from ',/Project';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
