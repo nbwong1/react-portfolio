73acb294b9cb79b4420cc0bd025f467d1d1991ed
nbwong <nicholas.b.wong@gmail.com>
2022-11-06 14:42:13 -0800

    updated project.js

---
+++ src/components/Project.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// add code for style for the project as cards, need to include the following: img, header(title), link to deployed app, link to github repo
+
+function Project() {
+    return (
+        <div style={styles.card}>
+            <div style={styles.heading}>
+
+            </div>
+        </div>
+    );
+}
+
+export default Project;
