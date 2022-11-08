5766af221dbdad6e48afcb0f1fc6b60b3e04aa57
nbwong <nicholas.b.wong@gmail.com>
2022-11-07 21:13:46 -0800

    updated bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import PortfolioContainer from './components/PortfolioContainer';
 import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const styles = {
     appStyles: {
