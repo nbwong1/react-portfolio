0ee9017e4ed28aa55536e4cace0cdd705cc98dad
nbwong <nicholas.b.wong@gmail.com>
2022-11-07 21:33:44 -0800

    updated App.js and index.js for bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './components/Header';
 import PortfolioContainer from './components/PortfolioContainer';
 import Footer from './components/Footer';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const styles = {
     appStyles: {
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 // using bootstrap?
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.render(<App />, document.getElementById('root'));
